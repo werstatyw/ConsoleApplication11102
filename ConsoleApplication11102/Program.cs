4abdaa35c4739d87bb28719c77e31cee95fcf5d0
hallgató <hallgat@LABOR115-17>
2017-10-11 08:33:30 +0200

    Add project files.

---
+++ ConsoleApplication11102/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = "abc12def111";
+
+            Console.WriteLine(s);
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsDigit(s[i])) count++;
+                if (s[i])
+                if (j == 0)
+                {
+                    g = R.Next(0, 10);
+                    s += g;
+                }
+                else
+                {
+                    g = R.Next(0, 26);
+                    g = g + 65;
+                    char c1 = (char)g;
+                    s +=
+                                    }
+            }
+            Console.WriteLine("The End");
+        }
+    }
+}
